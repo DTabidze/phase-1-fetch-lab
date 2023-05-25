32ed13f2efb16808d9bc7e9eef9b150904582450
Davit Tabidze <davidtabidzef1@gmail.com>
2023-05-25 15:16:09 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  //.then((json) => console.log(json[4].name))
+  .then((jsonFetch) => {renderBooks(jsonFetch)})
+
   
 }
 
@@ -7,9 +12,14 @@ function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
+    const bookURL = book.url;
+    // if (bookURL[bookURL.length-1] === "5") {
+    //   h2.innerHTML = `${book.name} This is 5Th Book`;
+    // } else {
     h2.innerHTML = book.name;
+ //   }
     main.appendChild(h2);
-  });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
